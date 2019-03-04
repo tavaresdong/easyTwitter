f8fe1a32d71416d80fe7940bd1dc6ee45dc78fb3
yuchen dong <yucdong@microsoft.com>
2019-03-04 21:21:15 +0800

    support unfollow user

--- entities/following.go
+++ entities/following.go
@@ -49,3 +49,38 @@ func FollowUser(client *redis.Client, uid string, otherUid string) error {
 
 	return nil
 }
+
+func UnfollowUser(client *redis.Client, uid string, otherUid string) error {
+	fkey1 := fmt.Sprintf("following:%s", uid)
+	fkey2 := fmt.Sprintf("followers:%s", otherUid)
+
+	if _, err := client.ZScore(fkey1, otherUid).Result(); err == redis.Nil {
+		return errors.New(fmt.Sprintf("%s did not follow %s", uid, otherUid))
+	}
+
+	pipe := client.Pipeline()
+	pipe.ZRem(fkey1, otherUid)
+	pipe.ZRem(fkey2, uid)
+	following := pipe.ZCard(fkey1)
+	followers := pipe.ZCard(fkey2)
+	statuses := pipe.ZRevRange(fmt.Sprintf("profile:%s", otherUid),
+		0, HOME_TIMELINE_SIZE - 1)
+
+	_, err := pipe.Exec()
+	fmt.Println(followers.Val(), err)
+
+	s := make([]interface{}, len(statuses.Val()))
+
+	for i := range statuses.Val() {
+		s[i] = statuses.Val()[i]
+	}
+	pipe = client.Pipeline()
+	pipe.HSet(fmt.Sprintf("users:%s", uid), "following", following.Val())
+	pipe.HSet(fmt.Sprintf("users:%s", otherUid), "followers", followers.Val())
+	if len(s) > 0 {
+		pipe.ZRem(fmt.Sprintf("home:%s", uid), s...)
+	}
+
+	pipe.Exec()
+	return nil
+}
--- entities/following_test.go
+++ entities/following_test.go
@@ -30,4 +30,43 @@ func TestFollowing(t *testing.T) {
 	messages, err := GetStatusMessage(client, uid1, "home:", 1, 30)
 	assert.Nil(t, err)
 	assert.Len(t, messages, 1)
+
+	messages, err = GetStatusMessage(client, uid1, "home:", 1, 30)
+	assert.Nil(t, err)
+	assert.Len(t, messages, 1)
+
+}
+
+func TestFollowAndUnfollow(t *testing.T) {
+	client := ConnectToRedis()
+	defer FlushRedis(client)
+
+	userLogin1 := "tavaresdong"
+	userLogin2 := "king of go"
+
+	uid1 := CreateUser(client, userLogin1, "yucdong")
+	uid2 := CreateUser(client, userLogin2, "king")
+
+	assert.NotEqual(t, "", uid1)
+	assert.NotEqual(t, "", uid2)
+
+	// The Followee posts a message
+	postedMessage := "What a beautiful day today"
+	_, err := CreateStatus(client, uid2, postedMessage)
+	assert.Nil(t, err)
+
+	// The Follower follows the Followee
+	err = FollowUser(client, uid1, uid2)
+	assert.Nil(t, err)
+
+	messages, err := GetStatusMessage(client, uid1, "home:", 1, 30)
+	assert.Nil(t, err)
+	assert.Len(t, messages, 1)
+
+	err = UnfollowUser(client, uid1, uid2)
+	assert.Nil(t, err)
+
+	messages, err = GetStatusMessage(client, uid1, "home:", 1, 30)
+	assert.Nil(t, err)
+	assert.Len(t, messages, 0)
 }
